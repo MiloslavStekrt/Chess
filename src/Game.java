3acfc21220eaa814bfaa6a9beb6e0a80ce8b2092
MiloslavStekrt <stekrt18@gmail.com>
2020-07-05 18:46:28 +0200

    Update function of Chess.

--- src/Game.java
+++ src/Game.java
@@ -2,16 +2,20 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Game extends JPanel {
-    public Game(){
-        // Generate window with 800x800 title >>> Chess
-        setSize(800, 800);
-        setVisible(true);
+    int width, height, border = 20;
+    public Game(int width, int height){
+        this.width = width - border;
+        this.height = height - 2 * border;
     }
-    //Generate Graphics
-
 
     @Override
     public void paint(Graphics g) {
-        //Draw rectangle
+        g.setColor(Color.BLACK);
+        g.fillRect(0,0,width, height);
+    }
+
+    @Override
+    public void repaint(Rectangle r) {
+        //s
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,63 +1,16 @@
 import javax.swing.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
-    static List<String> figure_ids = new ArrayList();
-    static List<Figurine> figurines = new ArrayList();
-    static String[][] playField = new String[8][8];
-    static boolean Start = true;
-    static int DefWarrior = 2,DefAles ;
-
-    //create Default figurine_id from Chess
-    public static void DefaultIds(){
-        figure_ids.add("King");
-        figure_ids.add("Queen");
-        for (int i = 0; i < 2; i++) {
-            figure_ids.add("Horseman");
-            figure_ids.add("Tower");
-            figure_ids.add("Archer");
-        }
-        for (int i = 0; i < 8; i++) {
-            figure_ids.add("Warrior");
-        }
-    }
-
-    public static void DefaultPosition(){
-
-    }
-
     public static void main(String[] args) {
-        DefaultIds();
-        for (int i = 0; i < figure_ids.size(); i++) {
-            if (figure_ids.get(i).contains("Warrior")){
-                // add object Figurine to list with name Warrior
-
-                if (Start == true)
-                    DefWarrior = 7;
-
-                playField[DefWarrior][i] = "Warrior";
-                figurines.add(new Figurine(DefWarrior, i, "Warrior"));
-            }else if(figure_ids.get(i).contains("Tower")){
-                if (Start == true){
-                    playField[1][8] = "Tower";
-                    figurines.add(new Figurine(1, 8, "Tower"));
-                    playField[8][8] = "Tower";
-                    figurines.add(new Figurine(8, 8, "Tower"));
-                }else{
-                    playField[1][1] = "Tower";
-                    figurines.add(new Figurine(1, 1, "Tower"));
-                    playField[8][1] = "Tower";
-                    figurines.add(new Figurine(8, 1, "Tower"));
-                }
-            }
-        }
-
-        /*
-        JFrame window = new JFrame();
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setTitle("Chess");
-        window.setContentPane(new Game());
-        */
+        //create JFrame with JPanel in game class
+        newWindow(800,800, "Chess");
+    }
+    public static void newWindow(int width, int height, String title){
+        JFrame frame = new JFrame(title);
+        frame.setSize(width,height);
+        frame.setContentPane(new Game(width,height-15));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        //frame.setResizable(false);
     }
 }
